0816308b0e16f5b2c2f47de15473d7678f9b02ca
T-Yoshiharu <tinpani138.haru@gmail.com>
2024-11-27 23:24:46 +0900

    log_v2 #3 #8 #ファイルへのログ出力機能完成

--- attendance_system/admin_services.py
+++ attendance_system/admin_services.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.logging import default_handler
 import logging
 import requests
+from datetime import datetime
 
 
 class DiscordHandler(logging.StreamHandler):
@@ -9,6 +10,8 @@ class DiscordHandler(logging.StreamHandler):
     def __init__(self, url):
         super(DiscordHandler, self).__init__()
         self.url = url
+        now = datetime.now().strftime('%Y.%m.%d-%H%M%S')
+        self.file = f"./attendance_system/logs/logging_{now}.log"
 
     def emit(self, record):
         msg = self.format(record)
@@ -20,3 +23,7 @@ class DiscordHandler(logging.StreamHandler):
         }
 
         requests.post(self.url, json=message)
+
+        # ログをファイルに出力する
+        with open(self.file, "a") as f:
+            f.write(text + "\n")
